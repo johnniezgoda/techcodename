9e3457a185d88ea973758cf10c4fd093db96d368
creator <niezjohn766@gmail.com>
2017-06-13 13:33:19 -0400

    hi

---
+++ CounselorHelper/CounselorHelper/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounselorHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double WaterTemp;
+            double AirTemp;
+            int ProbORain;
+            int TimeDay;
+
+            Console.Write("Please Enter water temp");
+            WaterTemp = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter Air Temp");
+            AirTemp = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter the rain probability");
+            ProbORain = Convert.ToInt32(Console.ReadLine());
+            Console.Write("PLease enter the time of day in a 24 hour format");
+            TimeDay = Convert.ToInt32(Console.ReadLine());
+
+            int NumActivities = 0;
+
+            if (TimeDay >= 12 && TimeDay <= 16 && WaterTemp > 70 && AirTemp > 80 && ProbORain < 30)
+            {
+                Console.WriteLine("Take the kids swimming!");
+                NumActivities++;
+            }
+            if (AirTemp > 60 && ProbORain < 50 && TimeDay >= 10 && TimeDay <= 18)
+            {
+                Console.WriteLine("Play Soccer!");
+                NumActivities++;
+            }
+            if (ProbORain > 60 || TimeDay > 18)
+            {
+                Console.WriteLine("Play board games");
+                NumActivities++;
+            }
+            if (TimeDay > 20)
+            {
+                Console.WriteLine("Watch Movies");
+                NumActivities++;
+            }
+            if (NumActivities == 0)
+            {
+                Console.WriteLine("No activities available.");
+            }
+        }
+    }
+}
---
+++ DayArray/DayArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentInClass = new string[5];
+            studentInClass[0] = "John";
+            studentInClass[1] = "Josh";
+            studentInClass[2] = "Evan";
+            studentInClass[3] = "Dylan";
+            studentInClass[4] = "Logan";
+
+            int i = 0;
+            while(i < studentInClass.Length)
+            {
+                Console.WriteLine(studentInClass[i]);
+                i++;
+            }
+        }
+    }
+}
